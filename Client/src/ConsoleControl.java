90f431d6199aaeeff5d7d9ad590e7777ee899071
Lutan <Lutan@cluffle.com>
2017-02-23 10:50:32 +0100

    First Client draft

---
+++ Client/src/ConsoleControl.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+
+public class ConsoleControl implements ControlInterface {
+	Scanner scanner = new Scanner(System.in);
+	
+	public ConsoleControl() {
+		/**
+		 * TODO
+		 * Connect
+		 * While connected, ask for input
+		 * if roaming, ask for roaming stuff
+		 * else ask for battle stuff
+		 * end loop on disconnect or notification of server
+		 */
+	}
+	
+	public String getUsername() {
+		System.out.print("Choose your username: ");
+		String input = scanner.next();
+		scanner.nextLine();
+		return input;
+	}
+	
+	public String getHost() {
+		System.out.print("Choose the host to connect to: ");
+		String input = scanner.next();
+		scanner.nextLine();
+		return input;
+	}
+	
+	public Integer getPort() {
+		System.out.print("Choose the port to connect to: ");
+		String input = scanner.next();
+		scanner.nextLine();
+		try {
+			Integer port = Integer.valueOf(input);
+			if (port.intValue() <= 0 || port.intValue() > 65536) {
+				System.out.println("That's not a valid port.");
+				return getPort();
+			}
+			return port;
+		} catch (NumberFormatException exception) {
+			System.out.println("That's not a valid port.");
+			return getPort();
+		}
+	}
+	
+	/**
+	 * Wait until user presses enter
+	 */
+	void waitUntilReady() {
+		System.out.print("Press ENTER once you're ready to play.");
+		scanner.nextLine();
+	}
+}
---
+++ Client/src/Game.java
@@ -0,0 +1,39 @@
+
+public class Game {
+	int health = 100;
+	
+	public Game() {
+		ConsoleControl control = new ConsoleControl();
+		
+		String host = control.getHost();
+		Integer port = control.getPort();
+		
+		//TODO Connect to host:port
+		
+		String username = control.getUsername();
+		/**
+		 * TODO
+		 * Send username to server, get confirmation
+		 * if username already used, repeat username question
+		 */
+		
+		control.waitUntilReady();
+		System.out.println("Ready");
+		
+		//While we're in the game
+		while (health > 0) {
+			
+		}
+	}
+	
+	public void updatePlayer() {
+		
+	}
+	
+	/**
+	 * Called by server, asks the player for his next move
+	 */
+	public void askForMove() {
+		
+	}
+}
