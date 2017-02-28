b2da5ad43b9b7e0c811719dd685eb5fbbc849596
Trayan7 <david_wolff@outlook.de>
2017-02-28 09:56:09 +0100

    with GUIClient

--- src/common/Main.java
+++ src/common/Main.java
@@ -1,13 +1,16 @@
 package common;
+
 import java.rmi.Naming;
 
 import server.Server;
 import client.Client;
+import client.GUIClient;
 
 public class Main {
 	public static void main(String[] args) {
-		if (args.length == 0 || !args[0].equals("server") && !args[0].equals("client")) {
-			System.out.println("You need to call this either as client or as server");
+		if (args.length == 0
+				|| !args[0].equals("server") && !args[0].equals("client") && !args[0].equals("GUIClient")) {
+			System.out.println("You need to call this as client, GUIClient or as server");
 			System.exit(0);
 		}
 		if (args[0].equals("server")) {
@@ -15,15 +18,22 @@ public class Main {
 			try {
 				Server server = new Server("random");
 				Naming.rebind(uri, server);
-				
+
 				server.start();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (args[0].equals("client")) {
+			try {
+				@SuppressWarnings("unused")
+				Client client = new Client();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		} else {
 			try {
 				@SuppressWarnings("unused")
-				Client client = new Client();
+				GUIClient client = new GUIClient();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
